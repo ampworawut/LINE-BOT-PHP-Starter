b56878738e44d075f6a0ba47580313ea2f9f9c57
ampworawut <worawut@hotmail.co.th>
2017-03-07 16:19:32 +0700

    sad

--- bot.php
+++ bot.php
@@ -16,9 +16,10 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
+			$replytext = "Got it";
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $replytext
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
