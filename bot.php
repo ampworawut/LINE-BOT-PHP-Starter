7c8932f689c01eb8ca2513f01b17eb0964e6a015
ampworawut <worawut@hotmail.co.th>
2017-12-15 23:44:06 +0700

    gsdf

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 			// Weather response
 			if (strpos($text,'weather')!== false){
         	$trimmed = str_replace("weather ", '', $text) ;
-			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
+			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q=".$trimmed."";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
     		$result = $ow_contents['weather']['main'];
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
 			// Oil response
 			if (strpos($text,'temp')!== false){
         	$trimmed = str_replace("temp ", '', $text) ;
-			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
+			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q=".$trimmed."";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
 			$result = $ow_contents['main']['temp'];
