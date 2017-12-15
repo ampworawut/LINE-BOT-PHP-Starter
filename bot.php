e8d39b82a0256995ac597a0a723b6bdf3ac9f91f
ampworawut <worawut@hotmail.co.th>
2017-12-15 21:43:11 +0700

    test
    test

--- bot.php
+++ bot.php
@@ -23,12 +23,23 @@ if (!is_null($events['events'])) {
 		}
 			// Weather response
 			if (strpos($text,'weather')!== false){
-        $trimmed = str_replace("weather ", '', $text) ;
-				$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
+        	$trimmed = str_replace("weather ", '', $text) ;
+			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
 				$replytext = $ow_contents['main']['temp'];
 			}
+			
+			// Weather response
+			if (strpos($text,'weather')!== false){
+        	$trimmed = str_replace("weather ", '', $text) ;
+			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
+    		$ow_response  = file_get_contents($ow_request);
+    		$ow_contents  = json_decode($ow_response, true);
+			$replytext = $ow_contents['main']['temp'];
+			}
+
+
 			// Build message to reply back
 		if ($replytext !== ""){
 			$messages = [
