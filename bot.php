1ec17a98bdc4245badebc80807b45bfcc4407cb6
ampworawut <worawut@hotmail.co.th>
2017-12-15 23:01:48 +0700

    sad

--- bot.php
+++ bot.php
@@ -27,8 +27,8 @@ if (!is_null($events['events'])) {
 			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
-			//$replytext = $ow_contents['weather']['main'];
-			$replytext = $ow_response;
+			$replytext = "Weather'.$ow_contents['weather']['main'].'";
+
 			}
 			
 			// Oil response
@@ -37,8 +37,8 @@ if (!is_null($events['events'])) {
 			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
-			//$replytext = $ow_contents['main']['temp'];
-			$replytext = $ow_response;
+			$replytext = "'.$ow_contents['main']['temp'].' Celcius";
+
 			}
 
 
