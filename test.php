8fe57255a55407561ff1030ea414f1e9b9f03a99
ampworawut <worawut@hotmail.co.th>
2017-12-15 23:10:11 +0700

    dasd

--- test.php
+++ test.php
@@ -1,12 +1,20 @@
 <?php
 
+$text = "weather bangkok"
 
-			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q=bangkok";
+			if (strpos($text,'temp')!== false){
+        	$trimmed = str_replace("temp ", '', $text) ;
+			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
-			$replytext = $ow_contents['main']['temp'];
+			$result = $ow_contents['main']['temp'];
+			$replytext = $result;
+			}
+
+			echo $trimmed;
+			echo "/n";
 
 			echo $replytext;
-
+			echo "/n";
 
                ?>
