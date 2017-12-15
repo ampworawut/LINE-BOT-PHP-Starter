c7c783cd7066f5a2416d0d127acb7b96d3138357
ampworawut <worawut@hotmail.co.th>
2017-12-16 00:12:54 +0700

    currency

--- bot.php
+++ bot.php
@@ -30,13 +30,22 @@ if (!is_null($events['events'])) {
     		$replytext = $ow_contents['weather'][0]['description'];
 			}
 			
-			// Oil response
+			// Temperature response
 			if (strpos($text,'temp')!== false){
         	$trimmed = str_replace("temp ", '', $text) ;
 			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q=".$trimmed."";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
 			$replytext = $ow_contents['main']['temp'];
+			}
+
+			// Currency response
+			if (strpos($text,'currency')!== false){
+        	$trimmed = str_replace("currency ", '', $text) ;
+			$ow_request = "https://api.fixer.io/latest?base=THB";
+    		$ow_response  = file_get_contents($ow_request);
+    		$ow_contents  = json_decode($ow_response, true);
+			$replytext = $ow_contents[$trimmed];
 			}
 
 
