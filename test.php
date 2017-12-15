95b9bf87856785a27d7625b98b957aac88c7bf64
ampworawut <worawut@hotmail.co.th>
2017-12-15 23:23:40 +0700

    wad

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
 
 $text = "weather bangkok";
 
-			if (strpos($text,'temp')!== false){
-        	$trimmed = str_replace("temp ", '', $text) ;
+			if (strpos($text,'weather')!== false){
+        	$trimmed = str_replace("weather ", '', $text) ;
 			$ow_request = "http://api.openweathermap.org/data/2.5/weather?appid=4170f37d550eea9a269901fe6eb64ed7&units=metric&q='.$trimmed.'";
     		$ow_response  = file_get_contents($ow_request);
     		$ow_contents  = json_decode($ow_response, true);
